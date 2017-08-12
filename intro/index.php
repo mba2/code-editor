142f86abadbf9a7bd8623f0f577e520772a4f339
Mario Brusarosco <mariobrusarosco@gmail.com>
2017-08-12 18:23:29 -0300

    Adjustements on intro page

---
+++ intro/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, maximum-scale=1, initial-scale=1">
+
+  <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+  <!-- Bootstrap-->
+  <!-- Animate CSS-->
+  <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+
+  <link rel="stylesheet" type="text/css" href="css/scss/normalize.css"> <!-- NORMALIZE CSS-->
+  <link rel="stylesheet" type="text/css" href="css/scss/my-page.css"> <!-- My CSS-->
+  <title>Code Editor - A Front End Playground</title>
+</head>
+<body>
+
+  <main id="main_wrapper" class="main_wrapper">
+
+    <header id="main_header" class="main_header">
+      <a href="index.php" id="portfolio_title" class="portfolio_title">
+         Code Editor - A Front End Playground
+        <i id="home_anchor" class="material-icons home_anchor">&#xE88A;</i>
+      </a>
+      </header>
+
+    <section class="projects_area">
+
+      <article id="code_editor" class="project code_editor">
+        <header class="project_header">
+          <h2 id="" class="project_title">CODE EDITOR</h2>
+        </header>
+
+        <div id="code-editor-content" class="project_content">
+          
+          <article id="" class="project_explanation">         
+            
+            <h1 id="" class="explanation_title">CODE EDITOR</h1>
+            <span class="explanation_horizontal_rule"></span>
+            <h2 id="" class="project_type">web || programação</h2>
+            <span class="explanation_horizontal_rule"></span>
+            
+            <section class="explanation_section intro_explanation">
+              <p class="">  Um Playground para Programação Front End.</p>
+            </section>
+            
+            <section class="explanation_section second_explanation">
+              <div id="" class="info_box">
+                <figure class="project_figure">
+                  <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="100px" height="100px" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+viewBox="0 0 100 100"
+ xmlns:xlink="http://www.w3.org/1999/xlink">
+ <defs>
+  <style type="text/css">
+   <![CDATA[
+    .str0 {stroke:tomato;stroke-width:3.99924}
+    .fil0 {fill:none}
+    .fil1 {fill:tomato;fill-rule:nonzero}
+   ]]>
+  </style>
+ </defs>
+ <g id="Layer_x0020_1">
+  <metadata id="CorelCorpID_0Corel-Layer"/>
+  <circle class="fil0 str0" cx="50" cy="50" r="47"/>
+  <polygon class="fil1" points="36,66 12,56 12,51 36,40 36,47 19,53 36,60 "/>
+  <polygon id="1" class="fil1" points="43,71 52,36 57,36 48,71 "/>
+  <polygon id="2" class="fil1" points="64,67 64,60 81,53 64,47 64,40 88,51 88,56 "/>
+ </g>
+
+                  </svg>
+                </figure>
+                <h3>CRIE SUA ESTRUTURA <span>HTML</span></h3>
+                <code class="html_code_example">
+                  <span class="tag div">div</span>
+                  <span class="tag p tab">p</span><span>Esse é um parágrafo</span><span class="tag p">/p</span>
+                  <span class="tag div">/div</span>
+                </code>
+              </div>            
+              
+              <div id="" class="info_box">
+                <figure class="project_figure">
+                  <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="100px" height="100px" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+                  viewBox="0 0 100 100"
+                   xmlns:xlink="http://www.w3.org/1999/xlink">
+                   <defs>
+                    <style type="text/css">
+                     <![CDATA[
+                      .str0 {stroke:tomato;stroke-width:3.99924}
+                      .fil0 {fill:none}
+                      .fil1 {fill:tomato;fill-rule:nonzero}
+                     ]]>
+                    </style>
+                   </defs>
+                   <g id="Layer_x0020_1">
+                    <metadata id="CorelCorpID_0Corel-Layer"/>
+                    <circle class="fil0 str0" cx="50" cy="50" r="47"/>
+                    <path class="fil1" d="M21 47l-3 -3c1,-1 2,-2 3,-3 1,-1 1,-1 1,-1 0,0 -1,0 -3,-1 -1,0 -2,0 -3,0l2 -4c2,0 3,1 5,2 0,-2 -1,-4 -1,-6l4 0c0,1 0,3 0,6 0,0 1,0 2,-1 1,0 2,-1 3,-1l2 4c-2,0 -4,1 -6,1l3 3c0,1 1,1 1,2l-4 2 -3 -5c-1,2 -2,3 -3,5z"/>
+                    <path id="1" class="fil1" d="M33 56l0 -6c1,0 2,0 2,0 1,0 1,-1 2,-2 0,0 0,-1 0,-2 1,-1 1,-2 1,-4 0,-3 0,-5 0,-6 0,-1 1,-2 2,-3 0,0 1,-1 3,-1 1,-1 2,-1 4,-1l1 0 0 6c-1,0 -3,0 -3,0 -1,0 -1,0 -1,1 0,0 0,1 0,2 0,1 0,3 -1,5 0,2 0,3 0,4 -1,1 -1,2 -1,2 -1,1 -2,1 -3,2 1,1 2,1 3,2 0,0 1,1 1,2 0,1 0,3 0,4 1,3 1,4 1,5 0,1 0,2 0,2 0,1 1,1 1,1 1,0 2,0 3,0l0 6 -1 0c-2,0 -4,0 -5,-1 -1,0 -2,-1 -2,-1 -1,-1 -2,-2 -2,-3 0,-1 0,-3 0,-5 0,-3 0,-5 -1,-6 0,-1 -1,-2 -1,-2 -1,-1 -2,-1 -3,-1z"/>
+                    <path id="2" class="fil1" d="M76 56c-1,0 -1,0 -2,0 -1,0 -1,1 -1,2 -1,0 -1,1 -1,2 0,1 0,2 0,4 0,3 -1,5 -1,6 0,1 -1,2 -1,3 -1,0 -2,1 -3,1 -1,1 -3,1 -5,1l-1 0 0 -6c2,0 3,0 3,0 1,0 1,0 1,-1 1,0 1,-1 1,-2 0,-1 0,-2 0,-5 0,-2 0,-3 0,-4 1,-1 1,-2 2,-2 0,-1 1,-1 2,-2 -1,-1 -2,-1 -3,-2 0,-1 -1,-2 -1,-4 0,-1 0,-3 0,-7 0,-1 0,-2 -1,-2 0,-1 0,-1 -1,-1 0,0 -1,0 -3,0l0 -6 1 0c2,0 4,0 5,1 1,0 2,1 3,1 0,1 1,2 1,3 0,1 1,3 1,5 0,3 0,5 0,6 0,1 1,2 1,2 1,1 2,1 3,1l0 6z"/>
+                   </g>
+                  </svg>
+
+                </figure>
+                <h3>USE <span>CSS</span> PARA FORMATAÇÃO</h3>
+                  <code class="css_code_example">
+                    <span class="open_curly_braces">div</span>
+                    <span class="css_decl">background-color: rgb(96, 125, 139);</span>
+                    <span class="close_curly_braces"></span>        
+                    <span class="open_curly_braces">p</span>
+                    <span class="css_decl">color: rgb(255, 255, 255);</span>
+                    <span class="close_curly_braces"></span>
+                  </code>
+              </div>
+              
+              <div id="" class="info_box">
+                <figure class="project_figure">
+                  <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="100px" height="100px" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+viewBox="0 0 100 100"
+ xmlns:xlink="http://www.w3.org/1999/xlink">
+ <defs>
+  <style type="text/css">
+   <![CDATA[
+    .str0 {stroke:tomato;stroke-width:3.99924}
+    .fil0 {fill:none}
+    .fil1 {fill:tomato}
+   ]]>
+  </style>
+ </defs>
+ <g id="Layer_x0020_1">
+  <metadata id="CorelCorpID_0Corel-Layer"/>
+  <circle class="fil0 str0" cx="50" cy="50" r="47"/>
+  <path class="fil1" d="M39 54c0,-2 0,-3 1,-5 1,-1 2,-2 2,-3 2,-1 5,-3 7,-3l2 0c4,0 7,3 9,6 1,3 1,6 0,9 -1,2 -2,3 -3,4 -3,3 -8,3 -12,1 -1,0 -2,-1 -3,-2 0,-1 0,-1 -1,-1 0,-1 0,-1 -1,-2 0,-1 -1,-2 -1,-4zm-3 -23c0,1 1,2 1,4 -1,3 -4,6 -8,5 -1,0 -1,0 -2,0l-1 1c0,2 -2,5 -2,6 1,0 2,1 3,1 4,2 4,9 0,11 -1,1 -2,1 -3,1 0,1 1,3 2,4 0,1 1,2 1,3 2,0 3,0 5,0 2,1 4,3 5,5 0,2 -1,3 -1,4 2,1 6,3 7,3 1,-3 5,-7 9,-5 2,1 3,2 4,3 0,0 1,2 1,2 1,0 5,-2 7,-3 0,-1 -1,-2 -1,-4 1,-4 5,-6 9,-5 0,0 0,0 1,0 0,-1 3,-5 3,-7 -4,0 -7,-4 -6,-9 1,0 1,-1 2,-2 1,-1 2,-2 4,-2 0,-1 -2,-4 -2,-5 -1,-1 -1,-2 -1,-2 -1,0 -1,0 -1,0 -1,0 -1,0 -1,0 -3,1 -4,0 -6,-2 -1,-1 -2,-3 -2,-5 0,-1 1,-1 1,-2 -1,-1 -1,-1 -3,-1 0,0 0,-1 -1,-1 0,0 -3,-1 -3,-1 0,3 -5,7 -9,5 -1,0 -2,0 -2,-1 -1,-1 -2,-2 -3,-4 0,0 -3,1 -3,1 -2,1 -3,1 -4,2z"/>
+ </g>
+
+                  </svg>
+
+                </figure>
+                <h3>ADICIONE INTERATIVIDADE COM <span>JAVASCRIPT</span></h3>
+                <code class="js_code_example">
+                  <span class="function">function nomeFunção(param_01){</span>
+                  <span class="function_content">console.log("Função chamada!");</span>
+                  <span class="function">}</span>
+                </code>
+              </div>
+            </section>
+                    
+            
+            
+<!--
+            <p>Use CSS para formatá-ĺa<p>
+            <p>Defina o comportamento de sua página usando Javascript!<p>
+            <p>Faça testes aqui, antes de modifica seu projeto principal</p>
+-->
+          </article>
+        </div>
+
+        <footer class="project_footer">
+           <!-- <a class="read_more_btn" href="#syntax">mais sobre</a> -->
+           <a id="code_editor_link" class="project_link" href="../home/" target="_blank">Abrir Projeto</a>
+        </footer>
+      </article>
+
+      <!-- <div id="fantasy_pes" class="project fantasy_pes">
+        <header class="project_header">
+          <h2 id="" class="project_title">Fantasy PES</h2>
+        </header>
+
+        <div id="" class="project_content">
+          <div id="" class="project_explanation">
+            <p>Crie sua liga<p>
+            <p>Crie seus times<p>
+            <p>Acompanhe a pontuação dos jogadores reais!<p>
+            <p>Transfira jogadores entre clubes</p>
+          </div>
+        </div>
+
+        <footer class="project_footer">
+           <!-- <a class="read_more_btn" href="#syntax">mais sobre</a> -->
+           <!-- <a id="fantasy_pes_link" class="project_link" href="#fantasy">Abrir Projeto</a>
+        </footer>
+      </div> -->
+
+
+    </section>
+
+   <!-- jQuery-->
+  <script src="js/jquery-2.2.1.min.js"></script>
+  <!-- My Script  - jQuery-->
+  <!-- <script src="js/my-page-f.js"></script> -->
+  <!-- My Script  - vanilha JS-->
+  <script src="js/my-page.js"></script>
+  <!-- Bootstrap Script-->
+  <script src=""></script>
+  <!-- viewportchecker Script-->
+  <script src="js/jquery.viewportchecker.min.js"></script>
+</body>
+</html>
