d826953474ab62554659a7a9949049c841f8d6c4
Mario Brusarosco <mariobrusarosco@gmail.com>
2018-01-08 18:39:42 -0200

    Cap 3 - completed

--- src/index.js
+++ src/index.js
@@ -5,24 +5,77 @@ if (module.hot) {
 
 import "knockout";
 
+let initialCats = [
+	{
+		clickCount : 0,
+		name : ("Kitty"),
+		imgSrc : ('src/img/main.png'),
+		imgAttr : 'Testing 01',
+		nickNames : ['Jesse', 'Skarlet', 'Pinkman' ]
+	},
+	{
+		clickCount : 0,
+		name : ("Kitty 2"),
+		imgSrc : ('src/img/webpack.svg'),
+		imgAttr : 'Testing 03',
+		nickNames : ['Gustavo', 'Frings', 'Los Poyos Hermanos' ]
+	},
+	{
+		clickCount : 0,
+		name : ("Kitty 3"),
+		imgSrc : ('src/img/angular.png'),
+		imgAttr : 'Testing 03',
+		nickNames : ['DEA', 'Hank', 'Rocks' ]
+	}
+];
+
+let Cat = function(data) {
+	let self = this;
+
+	self.clickCount = ko.observable(data.clickCount);
+	self.name = ko.observable(data.name);
+	self.imgSrc = ko.observable(data.imgSrc);
+	self.imgAttr = ko.observable(data.imgAttr);
+	self.nickNames = ko.observable(data.nickNames);
+
+	self.level = ko.computed(function() {
+		let level,
+			clicks = self.clickCount();
+
+		if(clicks < 10) level = 'infant';
+		else if(clicks < 20) level = 'child';
+		else if(clicks < 30) level = 'anoying';
+		else if(clicks < 40) level = 'teen';
+		else level = 'Adult';	
+
+		return level;
+	});
+};
+
+let ViewModel = function() {
+	let self = this;
+	self.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(currentCat) {
+		self.catList.push( new Cat( currentCat ) );
+	});
+	
+	this.currentCat = ko.observable( self.catList()[0] );
+	
+	this.incrementCounter = function() {
+		self.currentCat().clickCount( self.currentCat().clickCount() + 1 );
+	};
+
+	this.changeCat = function() {
+		self.currentCat(this);
+	};
+};
 
 
 window.addEventListener('DOMContentLoaded', () => {
-	console.log("I'm the one who knocks!");
+	'use strict';
 
-	let ViewModel = function(){
-		this.clickCount = ko.observable(0);
-		this.name = ko.observable("Kitty");
-		this.imgSrc = ko.observable('src/img/main.png');
-		this.imgAttr = ko.observable('Testing');
-		
-		this.incrementeCounter = function() {
-			this.clickCount(this.clickCount() + 1);
-		};
-	};
-
-
-
+	console.log("I'm the one who knocks!!!");
 
 	ko.applyBindings(new ViewModel());
 
